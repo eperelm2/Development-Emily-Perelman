fd0fce211009400d54cfe0ac5b49a9666b04bf37
eperelm2 <emily_perelman@brown.edu>
2024-03-30 19:12:31 -0400

    about to board plane

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,18 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1> Simple Spotify
+       </h1>
+
+       <div class= "song-list-side">
+        don't go breaking my heart
+       </div>
+
+       <div class="favorite-song-side">
+       </div>
+        
+       
       </header>
     </div>
   );
