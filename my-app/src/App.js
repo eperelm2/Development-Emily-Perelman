d17f7227c0587f801563963fcff82f4032de6af0
eperelm2 <emily_perelman@brown.edu>
2024-04-02 16:52:35 -0400

    removing working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
 import songData from "./assets/song-data.json";
 import SongItem from "./components/SongItem.js";
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faMinus } from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
+
+    const [favorites, setFavorites] = useState([])
+
+    const addToFavorites = (songName) => {
+        const updatedFavorites = [...favorites, songName]
+        setFavorites(updatedFavorites)
+      }
+
+    const removeFromFavorites = (songName) => {
+        const updatedFavorites = favorites.filter(item => item !== songName)
+        setFavorites(updatedFavorites)
+    }  
+
   return (
     <div className="App">
     <h1 className="header"> Simple Spotify</h1>
@@ -11,13 +28,23 @@ function App() {
       
             <div className="song-item-grid">
                 {songData.map((item, index) => (
-                <SongItem key={index} song={item} index={index} /> 
+                <SongItem key={index} song={item} index={index} addToFavorites={addToFavorites} /> 
               ))}
             </div>
     </div>
 
     <div class="favorite-song-side">
-      Favorite Songs
+
+      <h3>Favorite Songs</h3>
+      <p>Please add your favorite songs!</p>
+      {favorites.map((item,index) => (
+        <div className="favorites-list">
+            {index + 1}. {item}
+            <button className="add-to-cart" onClick={() => removeFromFavorites(item)}>
+                -
+            </button>
+        </div>
+      ))}
     </div>
     </div>
   );
--- my-app/src/components/SongItem.js
+++ my-app/src/components/SongItem.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
+
 const SongItem = ({ song, index, addToFavorites }) => {
     return (
       <div className="song-item">
@@ -7,6 +10,10 @@ const SongItem = ({ song, index, addToFavorites }) => {
             <p className="song-name">{song.name}</p>
             <p className="song-artist">{song.artist}</p>
         </div>
+
+        <button className="add-to-cart" onClick={() => addToFavorites(song.name)}>
+              <FontAwesomeIcon icon={faStar} />
+          </button>
         
         <p className="song-length">{song.length}</p>
         <p className="song-rating">{song.rating}</p>
