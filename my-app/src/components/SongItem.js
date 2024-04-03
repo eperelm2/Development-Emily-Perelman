30df1811d198eedccff9bc14af8cd8792d1fa6ab
eperelm2 <emily_perelman@brown.edu>
2024-04-03 12:08:20 -0400

    working on styling

--- my-app/src/components/SongItem.js
+++ my-app/src/components/SongItem.js
@@ -6,18 +6,31 @@ const SongItem = ({ song, index, addToFavorites }) => {
       <div className="song-item">
        
         <img src={song.image} alt="song-img" />
-        <div className = "name-and-artist">
-            <p className="song-name">{song.name}</p>
-            <p className="song-artist">{song.artist}</p>
-        </div>
 
-        <button className="add-to-cart" onClick={() => addToFavorites(song.name)}>
-              <FontAwesomeIcon icon={faStar} />
-          </button>
+      <div className = "artist-and-star">
+            <div className = "name-and-artist">
+                  <h3 className="song-name">{song.name}</h3>
+                  <p className="song-artist">{song.artist}</p>
+              </div>
+
+              <button className="add-to-favorites" onClick={() => addToFavorites(song.name)}>
+                    <FontAwesomeIcon icon={faStar} />
+                </button>
+      </div>
         
-        <p className="song-length">{song.length}</p>
-        <p className="song-rating">{song.rating}</p>
-        <p className="song-genre">{song.genre}</p>
+      <div className="additional-elements">
+          <div className="length-and-icon">
+            <p className="song-length">{song.length}</p>
+          </div>
+          <div className="rating-and-icon">
+            <p className="song-rating">{song.rating}</p>
+          </div>
+          <div className="genre-and-icon">
+            <p className="song-genre">{song.genre}</p>
+          </div>
+         
+      </div>
+       
         
         </div>
     );
